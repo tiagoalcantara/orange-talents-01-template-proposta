6f059659a2c8ec9eeeecd7729dc328f2964b2528
Tiago Alcantara <tiagofacampos@gmail.com>
2021-02-17 14:59:47 -0300

    010 - Login via senha

---
+++ src/main/java/br/com/zup/proposta/compartilhado/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package br.com.zup.proposta.compartilhado.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> {
+            authorizeRequests
+                    .antMatchers(HttpMethod.GET, "/actuator/**").hasAuthority("SCOPE_actuator:read")
+                    .antMatchers(HttpMethod.GET, "/proposta/**").hasAuthority("SCOPE_proposta:read")
+                    .antMatchers(HttpMethod.POST, "/proposta/**").hasAuthority("SCOPE_proposta:write")
+                    .antMatchers(HttpMethod.GET, "/cartao/**").hasAuthority("SCOPE_cartao:read")
+                    .antMatchers(HttpMethod.POST, "/cartao/**").hasAuthority("SCOPE_cartao:write")
+                    .anyRequest().authenticated();
+        })
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().exceptionHandling().authenticationEntryPoint(new JwtAuthenticationEntryPoint());
+    }
+
+    private static class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+        @Override
+        public void commence(HttpServletRequest httpServletRequest,
+                             HttpServletResponse httpServletResponse,
+                             AuthenticationException e) throws IOException, ServletException {
+            String json = "{\"erro\": \"Credenciais inválidas.\"}";
+            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            httpServletResponse.setContentType("application/json");
+            httpServletResponse.setLocale(LocaleContextHolder.getLocale());
+            httpServletResponse.getOutputStream()
+                               .write(json.getBytes(StandardCharsets.UTF_8));
+
+        }
+    }
+}
--- src/main/java/br/com/zup/proposta/proposta/PropostaController.java
+++ src/main/java/br/com/zup/proposta/proposta/PropostaController.java
@@ -7,14 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/proposta")
